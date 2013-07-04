1df869c239a14416567edc832fa2fb52f7db7d44
Birger Schmidt <bs-github@netgaroo.com>
2013-07-04 14:54:39 +1000

    debug hiera vars fed into gluster

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,8 @@ class gluster(
   include gluster::install
   include gluster::service
   include gluster::helper
-
+notify {"Running with \$peers ${peers} defined": withpath => true}
+notify {"Running with \$volumes ${volumes} defined": withpath => true}
   create_resources('gluster::peer', $peers)
   create_resources('gluster::volume', $volumes)
 }
