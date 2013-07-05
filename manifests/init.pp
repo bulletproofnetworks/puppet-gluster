4dd735b5587881f64d684f89291773a7b8508f98
Birger Schmidt <bs-github@netgaroo.com>
2013-07-05 16:30:49 +1000

    try to get to a working state with a hard coded hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,6 @@ notice("Running with \$peers ${peers} ID defined")
 notice("Running with \$volumes ${volumes} ID defined")
   #create_resources('gluster::peer', $peers)
   #create_resources('gluster::volume', $volumes)
-  create_resources('gluster::peer', ['modr-pmdh-static-01', 'modr-pmdh-static-02'])
+  create_resources('gluster::peer', {'modr-pmdh-static-01' => {} , 'modr-pmdh-static-02' => {} })
   create_resources('gluster::volume', $volumes)
 }
