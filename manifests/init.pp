efdc10bd31149f910500da92570f872a72a32ba1
Birger Schmidt <bs-github@netgaroo.com>
2013-07-05 16:40:41 +1000

    turns out I had to define the hostname even if the title is the same

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,12 +9,6 @@ class gluster(
   include gluster::install
   include gluster::service
   include gluster::helper
-notify {"Running with \$peers ${peers} defined": withpath => true}
-notify {"Running with \$volumes ${volumes} defined": withpath => true}
-notice("Running with \$peers ${peers} ID defined")
-notice("Running with \$volumes ${volumes} ID defined")
-  #create_resources('gluster::peer', $peers)
-  #create_resources('gluster::volume', $volumes)
-  create_resources('gluster::peer', {'modr-pmdh-static-01' => { hostname => 'modr-pmdh-static-01'} , 'modr-pmdh-static-02' => { hostname => 'modr-pmdh-static-02'} })
+  create_resources('gluster::peer', $peers)
   create_resources('gluster::volume', $volumes)
 }
