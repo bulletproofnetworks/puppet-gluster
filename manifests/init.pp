30b918ceb518338d38c41dd9a65bd1d58e235337
Birger Schmidt <bs-github@netgaroo.com>
2013-07-08 11:01:54 +1000

    debug try with hiera_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class gluster(
   include gluster::install
   include gluster::service
   include gluster::helper
-  #create_resources('gluster::peer', $peers)
-  create_resources('gluster::peer', {'modr-pmdh-static-01' => { hostname => 'modr-pmdh-static-01'} , 'modr-pmdh-static-02' => { hostname => 'modr-pmdh-static-02'} })
+  hiera_resources('gluster::peer', $peers)
+  #create_resources('gluster::peer', {'modr-pmdh-static-01' => { hostname => 'modr-pmdh-static-01'} , 'modr-pmdh-static-02' => { hostname => 'modr-pmdh-static-02'} })
   create_resources('gluster::volume', $volumes)
 }
