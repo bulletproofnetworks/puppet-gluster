538f0e1b7b95273a8d07d97d4e37ccd0ef86d3be
Birger Schmidt <bs-github@netgaroo.com>
2013-09-12 22:48:37 +0200

    try to avoid running exec resources on every run #2

--- manifests/peer.pp
+++ manifests/peer.pp
@@ -6,9 +6,10 @@ define gluster::peer(
 ) {
   # be relaxed, only grep for the hostname (not fqdn)
   $peergrep     = regsubst($hostname, '\..*$', '')
+  $fqdngrep     = regsubst($::fqdn, '\..*$', '')
 
   # Do unless current system is peer
-  if ( $hostname != $::fqdn ) {
+  if ( $peergrep != $fqdngrep ) {
     exec { "gluster peer probe $hostname":
       path        => '/bin:/sbin:/usr/bin:/usr/sbin',
       onlyif      => "! (gluster peer status | egrep -q '$peergrep')",
